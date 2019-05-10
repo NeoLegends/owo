b539093111b0aaa04f7e0abc7306585e3569f6d4
Moritz Gunz <moritz.gunz@gmail.com>
2019-05-10 17:17:36 +0200

    👌 Kawaii

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,22 @@ use std::env;
 use warp::{self, Filter};
 
 fn main() {
-    match env::args().nth(1) {
+    let mut args = env::args();
+    match args.nth(1) {
         Some(val) => match val.as_str() {
             "owo" => println!("uwu"),
             "uwu" => println!("owo"),
             "OwO" => println!("UwU"),
             "UwU" => println!("OwO"),
             "serve" => {
+                match args.next() {
+                    Some(ref v) if v == "please" || v == "pwease" => {},
+                    _ => {
+                        println!("Say pwease!");
+                        return;
+                    },
+                }
+
                 let handler = warp::any()
                     .map(|| "owo")
                     .map(|owo| warp::reply::with_header(owo, "Host", "Kawaii"));
