42f6205670e8f12ef460c472229f106fe9271913
Moritz Gunz <moritz.gunz@gmail.com>
2019-05-10 17:13:12 +0200

    💅 UwU

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::env;
 
+use warp::{self, Filter};
+
 fn main() {
     match env::args().nth(1) {
         Some(val) => match val.as_str() {
@@ -7,6 +9,12 @@ fn main() {
             "uwu" => println!("owo"),
             "OwO" => println!("UwU"),
             "UwU" => println!("OwO"),
+            "serve" => {
+                let handler = warp::any()
+                    .map(|| "owo")
+                    .map(|owo| warp::reply::with_header(owo, "Host", "Kawaii"));
+                warp::serve(handler).run(([127, 0, 0, 1], 8080));
+            },
             _ => println!("kawaii"),
         },
         _ => println!("q.q"),
