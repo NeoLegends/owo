8e198a46aa00ff7ac2c0838c357f2ba56155e511
Moritz Gunz <moritz.gunz@gmail.com>
2019-05-10 15:56:24 +0200

    😍 owo

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ fn main() {
         Some(val) => match val.as_str() {
             "owo" => println!("uwu"),
             "uwu" => println!("owo"),
+            "OwO" => println!("UwU"),
+            "UwU" => println!("OwO"),
             _ => println!("kawaii"),
         },
         _ => println!("q.q"),
